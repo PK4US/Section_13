61a6063a4403555cb9210389734449823c658ad3
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-11-09 15:51:41 +0200

    Урок 6. Перехват входящих SMS

--- Lesson_6/src/main/java/com/Lesson_6/MainActivity.java
+++ Lesson_6/src/main/java/com/Lesson_6/MainActivity.java
@@ -1,8 +1,11 @@
 package com.Lesson_6;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECEIVE_SMS}, 1);
+            return;
+        }
+
+        Bundle args = getIntent().getExtras();
+        if (args != null) {
+            ((TextView) findViewById(R.id.tv)).setText(args.getString("SMS"));
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) System.out.println("Разрешение получено!");
+            else System.out.println("Пользователь не разрешил перехватывать SMS из приложения!");
+        }
     }
 }
